ee9d688ffa25f2bf7cbd10f80f0b6d0f2d4c47d8
Silviu Costiniuc <silviucostiniuc@gmail.com>
2013-07-19 14:44:41 +0200

    creation fonctionalité suppresion log

--- log-delete.php
+++ log-delete.php
@@ -6,26 +6,17 @@ use Documents\LogComment;
 $boot = require 'bootstrap.php';
 $dm = $boot->getDocumentManager();
 
-// Debug::dump($dm);
+$log = $dm->getRepository('Documents\LogComment')->findOneBy(array('uid' => 2));
 
+var_dump($log);
 
-$log_info = array(
-  'cid' => 1,
-  'timestamp' => 1234567890,
-  'oldStatus'=> 1,
-  'newStatus' => 2,
-  'message' => 'Message test',
-  'uid' => 50,
-  'ip' => '192.168.0.5',
-);
+if ($log) {
+  $dm->remove($log);
+  $dm->flush();
+  echo 'Log deleted';
+}
+else {
+  echo 'Log not found';
+}
 
-$log = new LogComment($log_info);
-print_r($log);
-
-
-$dm->persist($log);
-print_r($log);
-
-$dm->flush();
-Debug::dump($log);
 
