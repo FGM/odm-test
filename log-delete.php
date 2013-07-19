126c4562dae4fd3210dcd537e7fc1d822b23b4d3
Silviu Costiniuc <silviucostiniuc@gmail.com>
2013-07-19 17:14:18 +0200

    ajouter de tests pour la suppresion des Logs

--- log-delete.php
+++ log-delete.php
@@ -6,17 +6,33 @@ use Documents\LogComment;
 $boot = require 'bootstrap.php';
 $dm = $boot->getDocumentManager();
 
-$log = $dm->getRepository('Documents\LogComment')->findOneBy(array('uid' => 2));
+$log = $dm->getRepository('Documents\LogComment')->findOneBy(array('uid' => (int) $argv[1]));
 
+var_dump($argv[1]);
 var_dump($log);
 
-if ($log) {
-  $dm->remove($log);
-  $dm->flush();
-  echo 'Log deleted';
-}
-else {
-  echo 'Log not found';
+if (!$log) {
+  echo 'Log inexistant pour les critieres selectiones.';
+  return FALSE;
 }
 
+// Recuperer l'id pour pouvoir tester l'existance après la suppresion.
+$lid = $log->_id;
+
+var_dump($lid);
+$dm->remove($log);
+$dm->flush();
+
+$boot2 = new Boot();
+
+$dm2 = $boot->getDocumentManager();
+$log2 = $dm->getRepository('Documents\LogComment')->findOneBy(array('_id' => $lid));
+
+
+if (is_null($log2)) {
+  echo 'Log supprimé';
+}
+else {
+  echo 'Erreur pendant la suppresion du Log';
+}
 
