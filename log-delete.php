60e0606bace23a2b9a0643d88221b1935c21741a
Silviu Costiniuc <silviucostiniuc@gmail.com>
2013-07-19 13:02:02 +0200

    New brack created

---
+++ log-delete.php
@@ -0,0 +1,31 @@
+<?php
+
+use Doctrine\Common\Util\Debug;
+use Documents\LogComment;
+
+$boot = require 'bootstrap.php';
+$dm = $boot->getDocumentManager();
+
+// Debug::dump($dm);
+
+
+$log_info = array(
+  'cid' => 1,
+  'timestamp' => 1234567890,
+  'oldStatus'=> 1,
+  'newStatus' => 2,
+  'message' => 'Message test',
+  'uid' => 50,
+  'ip' => '192.168.0.5',
+);
+
+$log = new LogComment($log_info);
+print_r($log);
+
+
+$dm->persist($log);
+print_r($log);
+
+$dm->flush();
+Debug::dump($log);
+
